a4b4f6fa8e5062c6ebf19b42613a88f12dfb198c
Pavitra-xworkz <73216121+Pavitra-xworkz@users.noreply.github.com>
2020-12-08 19:36:40 +0530

    Add files via upload

---
+++ SnackTester.java
@@ -0,0 +1,25 @@
+package com.xworkz.method.theater;
+
+import java.util.Iterator;
+
+import com.xworkz.method.theater.MovieHub;
+
+public class SnackTester {
+
+	
+	public static void main(String[] args) {
+		
+		String[] movieNames = { "PK", "Chennai Express", "KGF", "Departed", "Zodiac", "KGF2", "Inception",
+				"Interstellar", "Yajamana" };
+		
+		MovieHub movieHub=new MovieHub("PVR", 3, movieNames);
+		double price=movieHub.buySnacks("Chips", 3, true, null);
+		System.out.println("cost of snacks"+price);
+		String[] snacks=movieHub.getSnacksList();
+		for(int s=0;s<snacks.length;s++)
+		{
+			System.out.println(snacks[s]);
+		}
+		}
+}
+
