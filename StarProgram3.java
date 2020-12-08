47293c9d55793df5b0f26ab117723ef28ae0fae2
Pavitra-xworkz <73216121+Pavitra-xworkz@users.noreply.github.com>
2020-12-08 20:04:06 +0530

    Add files via upload

---
+++ StarProgram3.java
@@ -0,0 +1,14 @@
+package com.xworkz.Pattern;
+
+public class StarProgram3 {
+	
+	public static void main(String[] args) {
+		for (int i = 0; i <= 4; i++) {
+			for (int j = 4; j >= i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
